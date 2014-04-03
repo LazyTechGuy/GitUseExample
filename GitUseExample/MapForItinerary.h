4a697d374bb0a7ac53dd83b0fb97c2a53f879a1f
Chris Averill <chriseaverill@gmail.com>
2014-04-03 01:25:38 -0400

    Created new classes for Map Itinerary feature

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,16 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by Chris Averill on 4/3/14.
+//  Copyright (c) 2014 Chris Averill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface MapForItinerary : NSManagedObject
+
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by Chris Averill on 4/3/14.
+//  Copyright (c) 2014 Chris Averill. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+
+@implementation MapForItinerary
+
+
+@end
