b3f105631f7d04e757f97ec77bd3e5e64ec2a470
Chris Averill <chriseaverill@gmail.com>
2014-04-03 00:43:16 -0400

    Initial commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Chris Averill on 4/3/14.
+//  Copyright (c) 2014 Chris Averill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
